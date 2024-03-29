51cd9a61d0be0b94ddfdd875d8c9967569f939ee
AlanNegalho <alandevads@gmail.com>
2024-03-29 20:58:04 -0300

    add functions .js

--- js/script.js
+++ js/script.js
@@ -10,4 +10,26 @@ function topo() {
 
 //Validação de Login
 
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario == 'admin' && senha == '1234') {
+        window.location.href = "index.html";
+        logado = 1;
+    }
+    if (logado == 0) {
+        alert("Usuário ou senha incorretos");
+
+
+    }
+}
+
+
 //Ativar alert no botão cadastrar
+
+function cadastro() {
+    alert("Cadastro realizado com sucesso!");
+    window.location.href = "index.html";
+}
